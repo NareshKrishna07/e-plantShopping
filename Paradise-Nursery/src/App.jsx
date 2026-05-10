09e91ebf0a1f90a439cc40aab02a7fa5f238a25f
NareshKrishna07 <163556077+NareshKrishna07@users.noreply.github.com>
2026-05-10 21:33:40 +0530

    Update App.jsx

--- Paradise-Nursery/src/App.jsx
+++ Paradise-Nursery/src/App.jsx
@@ -1,13 +1,48 @@
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+
+import ProductList from "./components/ProductList";
 
 function App() {
+
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStarted = () => {
+
+    setShowProductList(true);
+  };
+
   return (
+
     <div className="landing-page">
-      <h1>Paradise Nursery</h1>
 
-      <p>Welcome to Paradise Nursery Plant Shop</p>
+      {
+        !showProductList ? (
 
-      <button>Get Started</button>
+          <div>
+
+            <h1>Paradise Nursery</h1>
+
+            <p>
+              Welcome to Paradise Nursery Plant Shop
+            </p>
+
+            <button
+              onClick={handleGetStarted}
+            >
+              Get Started
+            </button>
+
+          </div>
+
+        ) : (
+
+          <ProductList />
+
+        )
+      }
+
     </div>
   );
 }
